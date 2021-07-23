151aeba45ac67b56054c7f4dd7e58e410f622276
Aleksey Nikolaev <nikolaev@iprojects.ru>
2021-07-23 09:40:09 +0300

    env

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv'
 import { Telegraf } from 'telegraf'
 import express from 'express'
+
+dotenv.config()
 
 const expressApp = express()
 
@@ -16,40 +19,12 @@ expressApp.listen(port, () => {
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('quit', (ctx) => {
-  // Explicit usage
   ctx.telegram.leaveChat(ctx.message.chat.id)
-
-  // Using context shortcut
-  ctx.leaveChat()
 })
 
 bot.on('text', (ctx) => {
-  // Explicit usage
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-  // Using context shortcut
-  ctx.reply(`Hello ${ctx.state.role}`)
-})
-
-bot.on('callback_query', (ctx) => {
-  // Explicit usage
-  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
-
-  // Using context shortcut
-  ctx.answerCbQuery()
-})
-
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
-
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
+  console.log(ctx.update.message.from)
+  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.update.message.from.username}`)
 })
 
 bot.startPolling()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
